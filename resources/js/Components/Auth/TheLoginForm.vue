dd82b3191b2d0b0283562093a2bfaaf3ec859b96
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-06-24 09:48:39 +0300

    feat(TheLoginForm.vue): Create a component for the login form fix(PrimaryButton.vue): Change main color to sky fix(AuthLogin.vue): Use the TheLoginForm component

---
+++ resources/js/Components/Auth/TheLoginForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-full p-8 bg-white border rounded-lg shadow-lg border-sky-400 md:w-1/2 lg:w-1/3">
+            <form @submit.prevent="submit">
+                <div>
+                    <InputLabel for="email" value="Email" />
+
+                    <TextInput id="email" type="email" class="block w-full mt-1" v-model="form.email" required autofocus
+                        autocomplete="username" />
+
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="password" value="Password" />
+
+                    <TextInput id="password" type="password" class="block w-full mt-1" v-model="form.password" required
+                        autocomplete="current-password" />
+
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="block mt-4">
+                    <label class="flex items-center">
+                        <Checkbox name="remember" v-model:checked="form.remember" />
+                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <Link v-if="canResetPassword" :href="route('password.request')"
+                        class="text-sm text-gray-600 underline rounded-md hover:text-sky-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Forgot your password?
+                    </Link>
+
+                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Sign in
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script setup>
+import Checkbox from '@/Components/Checkbox.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import CustomButton from '@/Components/Commons/CustomButton.vue';
+import { Link, useForm } from '@inertiajs/vue3';
+
+defineProps({
+    canResetPassword: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+<style scoped></style>
