80e2751b9a4850008e81c0d4fa7e1ae820ca6978
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-06-24 20:21:22 +0300

    fix(CreateQuestion.vue): Use AuthenticatedLayout instead of QuestLayout

--- resources/js/Pages/Questions/CreateQuestion.vue
+++ resources/js/Pages/Questions/CreateQuestion.vue
@@ -1,14 +1,14 @@
 <script setup>
-import GuestLayout from "@/Layouts/GuestLayout.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import TheCreateQuestionForm from "@/Components/Questions/TheCreateQuestionForm.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <GuestLayout>
+    <AuthenticatedLayout>
 
         <Head title="Create question" />
 
-        <TheCreateQuestionForm class="my-20"/>
-    </GuestLayout>
+        <TheCreateQuestionForm class="my-20" />
+    </AuthenticatedLayout>
 </template>
