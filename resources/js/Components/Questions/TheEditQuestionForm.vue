4c9cac9f6ba0cc5a528951b30a05b446db12276b
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-06-24 20:36:45 +0300

    feat(TheEditQuestionForm.vue): Create a component for editing a question fix(EditQuestion.vue): Use the TheEditQuestionForm component

---
+++ resources/js/Components/Questions/TheEditQuestionForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-full p-8 bg-white border rounded-lg shadow-lg border-sky-400 md:w-1/2 lg:w-1/3">
+            <form @submit.prevent="submit">
+                <h1 class="text-2xl font-bold text-center text-gray-800 mb-7">Edit a question</h1>
+
+                <div>
+                    <InputLabel for="content" value="Content" />
+
+                    <textarea id="content" class="w-full p-2 mt-1 border border-gray-300 rounded-md min-h-[7em]"
+                        v-model="form.content" required autofocus autocomplete="content" />
+
+                    <InputError class="mt-2" :message="form.errors.content" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="difficulty" value="Difficulty" />
+
+
+                    <div class="inline-block w-full mt-1">
+                        <select id="difficulty"
+                            class="block w-full px-4 py-2 pr-8 bg-white border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-primary focus:border-primary-dark"
+                            v-model="form.difficulty">
+                            <option value="easy">Easy</option>
+                            <option value="medium">Medium</option>
+                            <option value="hard">Hard</option>
+                        </select>
+                    </div>
+
+                    <InputError class="mt-2" :message="form.errors.difficulty" />
+                </div>
+
+                <div class="flex items-center justify-end mt-8">
+                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Update
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { useForm } from "@inertiajs/vue3";
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+
+const form = useForm({
+    content: props.question.content,
+    difficulty: props.question.difficulty,
+});
+
+const submit = () => {
+    form.put(route("questions.update", props.question.id), {});
+};
+</script>
+
+<style scoped></style>
--- resources/js/Pages/Questions/EditQuestion.vue
+++ resources/js/Pages/Questions/EditQuestion.vue
@@ -1,55 +1,16 @@
 <template>
-    <form @submit.prevent="submit">
-        <div>
-            <InputLabel for="content" value="Content" />
+    <AuthenticatedLayout>
 
-            <TextInput
-                id="content"
-                type="text"
-                class="block w-full mt-1"
-                v-model="form.content"
-                required
-                autofocus
-                autocomplete="content"
-            />
+        <Head title="Edit a question" />
 
-            <InputError class="mt-2" :message="form.errors.content" />
-        </div>
-
-        <div>
-            <InputLabel for="difficulty" value="Difficulty" />
-
-            <TextInput
-                id="difficulty"
-                type="text"
-                class="block w-full mt-1"
-                v-model="form.difficulty"
-                required
-                autofocus
-                autocomplete="difficulty"
-            />
-
-            <InputError class="mt-2" :message="form.errors.difficulty" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <PrimaryButton
-                class="ml-4"
-                :class="{ 'opacity-25': form.processing }"
-                :disabled="form.processing"
-            >
-                Update
-            </PrimaryButton>
-        </div>
-    </form>
+        <TheEditQuestionForm :question="question" class="" />
+    </AuthenticatedLayout>
 </template>
 
 <script setup>
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
-import { useForm } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import TheEditQuestionForm from "@/Components/Questions/TheEditQuestionForm.vue";
+import { Head } from "@inertiajs/vue3";
 
 const props = defineProps({
     question: {
@@ -57,15 +18,6 @@ const props = defineProps({
         required: true,
     },
 });
-
-const form = useForm({
-    content: props.question.content,
-    difficulty: props.question.difficulty,
-});
-
-const submit = () => {
-    form.put(route("questions.update", props.question.id), {});
-};
 </script>
 
 <style scoped></style>
