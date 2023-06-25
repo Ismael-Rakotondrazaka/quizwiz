9fb7e700cbab14090cc4dfc039c24bd341d53dac
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-06-25 11:28:03 +0300

    feat(TheCreateAnswerForm.vue): Create a componentn for creating an answer fix(CreateAnswer.vue): Use the TheCreateAnswerForm component

---
+++ resources/js/Components/Answers/TheCreateAnswerForm.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-full p-8 bg-white border rounded-lg shadow-lg border-sky-400 md:w-1/2 lg:w-2/3">
+            <form @submit.prevent="submit">
+                <h1 class="text-2xl font-bold text-center text-gray-800 mb-7">Create an answer</h1>
+
+                <div>
+                    <InputLabel for="question" value="Question" />
+
+                    <textarea id="question" readonly
+                        class="w-full p-2 mt-1 border border-gray-300 bg-gray-100 rounded-md min-h-[7em] cursor-not-allowed focus:border-gray-300"
+                        v-model="props.question.content" required autofocus autocomplete="content" />
+
+                    <InputError class="mt-2" :message="form.errors.content" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="content" value="Answer" />
+
+                    <table class="min-w-full mt-1 bg-white border border-gray-300">
+                        <thead>
+                            <tr>
+                                <th class="w-full px-4 py-2 border-b">Content</th>
+                                <th class="px-4 py-2 border-b">Correct</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-slate-50">
+                                <td class="px-4 py-2 border-b">
+                                    <textarea id="answer" class="w-full min-h-[5em] p-2 border border-gray-300 rounded-md"
+                                        v-model="form.content" required></textarea>
+                                </td>
+                                <td class="px-4 py-2 text-center border-b">
+                                    <input id="is-correct" type="checkbox" v-model="form.is_correct" class="w-7 h-7" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <InputError class="mt-2" :message="form.errors.content" />
+
+                <div class="flex items-center justify-end mt-9">
+                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Create
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Checkbox from "@/Components/Checkbox.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+
+const form = useForm({
+    content: "",
+    is_correct: false,
+});
+
+const submit = () => {
+    form.post(route("answers.store", props.question), {});
+};
+</script>
+
+<style scoped></style>
--- resources/js/Pages/Answers/CreateAnswer.vue
+++ resources/js/Pages/Answers/CreateAnswer.vue
@@ -1,11 +1,7 @@
 <script setup>
-import Checkbox from "@/Components/Checkbox.vue";
-import GuestLayout from "@/Layouts/GuestLayout.vue";
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
-import { Head, Link, useForm } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import TheCreateAnswerForm from "@/Components/Answers/TheCreateAnswerForm.vue";
+import { Head, } from "@inertiajs/vue3";
 
 const props = defineProps({
     question: {
@@ -13,56 +9,13 @@ const props = defineProps({
         required: true,
     },
 });
-
-const form = useForm({
-    content: "",
-    is_correct: false,
-});
-
-const submit = () => {
-    form.post(route("answers.store", props.question), {});
-};
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Create answer" />
+    <AuthenticatedLayout>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="content" value="Content" />
+        <Head title="Create an answer" />
 
-                <TextInput
-                    id="content"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.content"
-                    required
-                    autofocus
-                    autocomplete="content"
-                />
-
-                <InputError class="mt-2" :message="form.errors.content" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="is_correct" v-model:checked="form.is_correct" />
-                    <span class="ml-2 text-sm text-gray-600">Is correct</span>
-                </label>
-            </div>
-
-            {{ form.errors.answers }}
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton
-                    class="ml-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Create
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        <TheCreateAnswerForm :question="question" />
+    </AuthenticatedLayout>
 </template>
