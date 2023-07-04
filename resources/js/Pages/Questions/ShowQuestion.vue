623c07a6f076f135ffbdbefcf159106d39fee5ef
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-07-04 19:34:19 +0300

    fix(ShowQuestion.vue): Add margin to the main content

--- resources/js/Pages/Questions/ShowQuestion.vue
+++ resources/js/Pages/Questions/ShowQuestion.vue
@@ -1,38 +1,15 @@
 <template>
     <AuthenticatedLayout>
-
         <Head :title="title" />
 
-        <ShowQuestionItem :question="question" />
+        <ShowQuestionItem :question="question" class="my-20" />
     </AuthenticatedLayout>
-
-    <!-- <ul>
-        'ses'
-        <li>
-            <h1>{{ index + 1 }}. {{ question.content }}</h1>
-            <ul>
-                <li
-                    v-for="(answer, index2) in question.answers"
-                    :key="answer.id"
-                >
-                    <label>
-                        <input
-                            type="radio"
-                            :value="answer.id"
-                            v-model="answers[index]['answer_id']"
-                        />
-                        {{ index2 }}. {{ answer.content }}
-                    </label>
-                </li>
-            </ul>
-        </li>
-    </ul> -->
 </template>
 
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import ShowQuestionItem from "@/Components/Questions/ShowQuestionItem.vue";
-import { Link, Head } from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 import { computed } from "vue";
 
 const props = defineProps({
@@ -42,7 +19,9 @@ const props = defineProps({
     },
 });
 
-const title = computed(() => "Question | " + props.question.content.slice(0, 50) + "...");
+const title = computed(
+    () => "Question | " + props.question.content.slice(0, 50) + "..."
+);
 </script>
 
 <style scoped></style>
