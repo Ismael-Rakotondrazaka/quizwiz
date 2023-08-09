481b2cf1a1030be7b38459da0f43d85114d4560d
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-08-09 08:02:10 +0300

    feat(TheHeroSection.vue): Make responsive

--- resources/js/Components/Welcome/TheHeroSection.vue
+++ resources/js/Components/Welcome/TheHeroSection.vue
@@ -1,28 +1,41 @@
 <template>
-    <main class="relative min-h-[100vh] flex items-center justify-around">
-        <img src="@/assets/images/illustration_question.svg" alt="" class="w-1/3 max-w-xl" />
-        <div>
-            <h1 id="" class="mb-16 font-bold text-white text-8xl text-shadow">Test Your Coding<br><span id="typing" /></h1>
-            <Link :href="href"
-                class="px-10 py-4 text-3xl text-white uppercase border-2 rounded-full bg-sky-800/80 fond-bold">
-            Start now</Link>
+    <main
+        class="relative min-h-[100vh] flex items-center justify-around flex-wrap-reverse ld:flex-no-wrap"
+    >
+        <img
+            src="@/assets/images/illustration_question.svg"
+            alt=""
+            class="w-[90%] max-w-xl lg:w-1/3"
+        />
+        <div class="flex items-center justify-center">
+            <div class="w-full mx-auto my-10 md:w-fit lg:my-0">
+                <h1
+                    id=""
+                    class="text-4xl font-bold text-white md:text-6xl mb-7 lg:mb-16 xl:text-7xl text-shadow"
+                >
+                    Test Your Coding<br /><span id="typing" />
+                </h1>
+                <Link
+                    :href="href"
+                    class="px-5 py-2 text-white uppercase border-2 rounded-full md:px-7 md:py-3 lg:px-10 lg:py-4 text-md lg:text-3xl bg-sky-800/80 fond-bold"
+                >
+                    Start now
+                </Link>
+            </div>
         </div>
     </main>
 </template>
 
 <script setup>
-import Typed from 'typed.js';
+import Typed from "typed.js";
 import { onMounted, ref, computed } from "vue";
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
 
 const typed = ref(null);
 
 onMounted(() => {
-    typed.value = new Typed('#typing', {
-        strings: [
-            "Superpowers",
-            "Skills"
-        ],
+    typed.value = new Typed("#typing", {
+        strings: ["Superpowers", "Skills"],
         typeSpeed: 100,
         loop: true,
         loopCount: Infinity,
@@ -37,10 +50,12 @@ const props = defineProps({
         type: Boolean,
         required: false,
         default: false,
-    }
+    },
 });
 
-const href = computed(() => props.isAuthenticated ? route('dashboard') : route('login'));
+const href = computed(() =>
+    props.isAuthenticated ? route("dashboard") : route("login")
+);
 </script>
 
 <style scoped>
