9d2969eaa4cb51411e428adc5ba877eb85cb4796
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-08-09 10:54:08 +0300

    feat(TheCreateSessionForm.vue): Add title

--- resources/js/Components/Sessions/TheCreateSessionForm.vue
+++ resources/js/Components/Sessions/TheCreateSessionForm.vue
@@ -4,7 +4,12 @@
             <CustomStepper
                 :steps-count="stepsCount"
                 :currentStep="currentStep"
+                class="mb-7"
             />
+
+            <h1 class="text-2xl font-bold text-center text-gray-800">
+                Select the right answer
+            </h1>
 
             <QuestionList
                 @answers:update="onAnswerUpdateHandler"
